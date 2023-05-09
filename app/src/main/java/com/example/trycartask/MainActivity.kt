4641fb52e4fcf1b5b0dc12867364ff782927c82b
mark.khalil <mark.khalil@hmaserv.com>
2023-05-09 20:10:35 +0300

    Added Required Libraries and dependencies

--- app/src/main/java/com/example/trycartask/MainActivity.kt
+++ app/src/main/java/com/example/trycartask/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
+
 import android.view.Menu
 import android.view.MenuItem
 import com.example.trycartask.databinding.ActivityMainBinding
