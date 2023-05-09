98f08e5fc7bcb3ec5660dbb6eb7d1f887792dae9
mark.khalil <mark.khalil@hmaserv.com>
2023-05-09 21:23:17 +0300

    Added Posts Models

---
+++ app/src/main/java/com/example/trycartask/data/models/Posts.kt
@@ -0,0 +1,8 @@
+package com.example.trycartask.data.models
+
+
+import androidx.room.Entity
+import androidx.room.Index
+import com.google.gson.annotations.SerializedName
+
+class Posts : ArrayList<PostsItem>()
---
+++ app/src/main/java/com/example/trycartask/data/models/PostsItem.kt
@@ -0,0 +1,25 @@
+package com.example.trycartask.data.models
+
+
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+import java.util.*
+@Entity(tableName = "post_table", indices = [Index(value = ["title"], unique = true)])
+
+data class PostsItem(
+    @SerializedName("body")
+    val body: String?,
+    @SerializedName("title")
+    val title: String?,
+    @SerializedName("userId")
+    val userId: Int?
+):java.io.Serializable{
+    @PrimaryKey(autoGenerate = true)
+    @SerializedName("id")
+    var id: Int = 0
+    var bookmarked: Boolean = false
+    var entryDate = Date().time
+
+}
